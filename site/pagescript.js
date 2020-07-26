1e1f0532aa4873c71c100c94ae13889b92cc3f34
ms2459 <martha.a.stevens@outlook.com>
2020-07-26 22:55:28 +0100

    further interactive changes

--- site/pagescript.js
+++ site/pagescript.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded",
 		// removed from the list of events when each option is chose
 		var logic_flow = {
 							//How much of training for running
-							a_1: ["SS", "LS", "MD", "H", "HZJ", "M"], 
-							a_2: ["MD", "HT", "J"],
-							a_3: ["HT", "J", "HZJ", "PV", "HJ"],
+							a_1: ["c", "SS", "LS", "MD", "H", "HZJ", "M"], 
+							a_2: ["b", "MD", "HT", "J"],
+							a_3: ["b", "HT", "J", "HZJ", "PV", "HJ"],
 							
 							//levels of running intensity
-							b_1: ["SS", "LS", "MD", "H",  "M"], 
-							b_2: ["MD"],
-							b_3: ["H", "HZJ", "PV"],
+							b_1: ["c","SS", "LS", "MD", "H",  "M"], 
+							b_2: ["c","MD"],
+							b_3: ["c","H", "HZJ", "PV"],
 
 							//strength focus
 							c_1: ["HT"],
@@ -28,71 +28,80 @@ document.addEventListener("DOMContentLoaded",
 							d_3: ["SS", "LS", "MD"]
 		};
 
+		var question_store = {
+								a: "running question",
+								b: "intensity question",
+								c: "strength question",
+								d: "technical question",
+								end: "end screen"
+		};
+
 		//find out where we are in the logic flow so we know what events to eliminate
 		function choose_elim(question_id, chosen_option){
 			if (question_id === "a"){
-				if chosen_option === "optn_1"{
+				if (chosen_option === "1"){
 					var logic_array = logic_flow.a_1;					
 				};
 
-				if chosen_option === "optn_2"{
+				if (chosen_option === "2"){
 					var logic_array = logic_flow.a_2;					
 				};
 
-				if chosen_option === "optn_3"{
+				if (chosen_option === "3"){
 					var logic_array = logic_flow.a_3;					
 				};
 			};
 
 			if (question_id === "b"){
-				if chosen_option === "optn_1"{
-					var logic_array = logic_flow.a_1;					
+				if (chosen_option === "1"){
+					var logic_array = logic_flow.b_1;					
 				};
 
-				if chosen_option === "optn_2"{
-					var logic_array = logic_flow.a_2;					
+				if (chosen_option === "2"){
+					var logic_array = logic_flow.b_2;					
 				};
 						
-				if chosen_option === "optn_3"{
-					var logic_array = logic_flow.a_3;					
+				if (chosen_option === "3"){
+					var logic_array = logic_flow.b_3;					
 				};
 			};
 
 			if (question_id === "c"){
-				if chosen_option === "optn_1"{
-					var logic_array = logic_flow.a_1;					
+				if (chosen_option === "1"){
+					var logic_array = logic_flow.c_1;					
 				};
 
-				if chosen_option === "optn_2"{
-					var logic_array = logic_flow.a_2;					
+				if (chosen_option === "2"){
+					var logic_array = logic_flow.c_2;					
 				};
 						
-				if chosen_option === "optn_3"{
-					var logic_array = logic_flow.a_3;					
+				if (chosen_option === "3"){
+					var logic_array = logic_flow.c_3;					
 				};
 			};
 
 			if (question_id === "d"){
-				if chosen_option === "optn_1"{
-					var logic_array = logic_flow.a_1;					
+				if (chosen_option === "1"){
+					var logic_array = logic_flow.d_1;					
 				};
 
-				if chosen_option === "optn_2"{
-					var logic_array = logic_flow.a_2;					
+				if (chosen_option === "2"){
+					var logic_array = logic_flow.d_2;					
 				};
 						
-				if chosen_option === "optn_3"{
-					var logic_array = logic_flow.a_3;					
+				if (chosen_option === "3"){
+					var logic_array = logic_flow.d_3;					
 				};
 			};
 
 			return logic_array;
+
 		};
 
-		function elim(selected_array){
-			for (i = 0; i < selected_array.length; i++){
+		function elim(logic_path){
+			for (i = 0; i < logic_path.length; i++){
 				for (j = 0; j < my_event_list.length; j++){
-					if (selected__array[i] === my_event_list[j]){
+					if (logic_path[i] === my_event_list[j]){
 						my_event_list.splice(j, 1);
 					};
 				};
@@ -100,38 +109,73 @@ document.addEventListener("DOMContentLoaded",
 			return my_event_list;
 		};
 
-		//function change_questions(selected_array){
-
-		//};
+		function change_questions(logic_path){
+			var next_question = logic_path[0];
+			if (next_question === "a"){
+				document.getElementById("question_text")
+				  .textContent = question_store.a;
+				document.getElementById("question_no")
+				  .textContent = next_question;
+			};	
+			if (next_question === "b"){
+				document.getElementById("question_text")
+				  .textContent = question_store.b;
+				document.getElementById("question_no")
+				  .textContent = next_question;
+			};		
+			if (next_question === "c"){
+				document.getElementById("question_text")
+				  .textContent = question_store.c;
+				document.getElementById("question_no")
+				  .textContent = next_question;
+			};
+			if (next_question === "d"){
+				document.getElementById("question_text")
+				  .textContent = question_store.d;
+				document.getElementById("question_no")
+				  .textContent = next_question;
+			};
+			if (next_question === "end"){
+				document.getElementById("question_text")
+				  .textContent = question_store.end;
+				document.getElementById("question_no")
+				  .textContent = next_question;
+			};
+			
+		};
 
 		function next_q (event){
-			var chosen_option = this.value;
+			var chosen_option = this.textContent;
 			// check wheather value is returning as I thought.
-			console.log(chosen_option);
 			// Identify which question we are on
 			var question_id = 
 			  document.getElementById("question_no").textContent; 
 
+			console.log("question id is " + question_id);
+			console.log("chosen option is " +  chosen_option);
+
 			//use the retrieved information to decide what to do next
 			var logic_path = choose_elim(question_id, chosen_option);
+			console.log( "logic path is " + logic_path);
 			
 			//alter the list of events based on the logic path identified
 			var remaining_events = elim(logic_path);
-
-			// change_questions(logic_path);
+			console.log(remaining_events);
+			console.log(document.getElementById("question_text"));
+			change_questions(logic_path);
 
 		};
 		
 
-		document.querySelector("optn_1")
+		document.querySelector("p1")
 		  .addEventListener("click", next_q);
 
-		document.querySelector("optn_2")
+		document.querySelector("p2")
 		  .addEventListener("click", next_q);
 
-		document.querySelector("optn_3")
+		document.querySelector("p3")
 		  .addEventListener("click", next_q);
-	};
+	});
 
 
 
